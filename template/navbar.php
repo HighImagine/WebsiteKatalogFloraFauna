c0dd83527322be1cb094c54d2fe2d04ac494dfbb
A Faruq <afaruqfirdaus@gmail.com>
2026-05-24 21:49:28 +0700

    changes

---
+++ template/navbar.php
@@ -0,0 +1,30 @@
+<div class="header">
+        <div class="Search_bar">
+            <form action="Search_menu.html"><input type="text" placeholder="Cari Disini..."></form>
+            <div class="logosearch"></div>
+        </div>
+
+        <div class="logowallacea"></div>
+
+        <a href="Profil_user.html">
+            <div class="logoprofil"></div>
+        </a>
+
+        <div class="menu">
+            <a href="homepage-after.html">
+                <p>Homepage</p>
+            </a>
+            <a href="category.html">
+                <p>Category</p>
+            </a>
+            <a href="information.html">
+                <p>Information</p>
+            </a>
+            <a href="comunity.html">
+                <p>Comunity</p>
+            </a>
+            <a href="wishlist.html">
+                <p>My List</p>
+            </a>
+        </div>
+    </div>
